9ad1cf28311d8240da32c742eb587bad5333a858
nazmul_khan <nazmul.khan121@gmail.com>
2026-06-05 20:17:05 +0600

    practice DOM manipulation

--- script.js
+++ script.js
@@ -15,4 +15,31 @@ test();
   var speed = 100;
 }
 console.log(speed);
-console.log(height);
+// console.log(height);
+
+var a = 10;
+
+function test() {
+  var b = 20;
+
+  if (true) {
+    var c = 30;
+    let d = 40;
+  }
+
+  console.log(a);
+  console.log(b);
+  console.log(c);
+  //   console.log(d);
+}
+
+test();
+
+var x = 10;
+
+function test2() {
+  console.log('test2', x);
+
+  var x = 20;
+}
+test2();
